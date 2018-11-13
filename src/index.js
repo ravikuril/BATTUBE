ee11d75484f7ccfb90b3f69b0e7befb903557738
Ravi.Kuril <ravi.kuril@team.telstra.com>
2018-11-14 00:25:33 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,42 @@
+import React,{ Component } from 'react';
+import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+import SearchBar from './components/search_bar'; //importing componet from the search_bar.js
+
+const API_KEY='AIzaSyAWU2gcLpeDW2SJK9aDPOthNNr72zsZa38';
+
+class App extends Component {
+	constructor(props){
+		super(props);
+
+		this.state={videos:[],
+			selectedVideo: null
+		};
+
+		this.videoSearch('batman vs superman');
+	}
+
+	videoSearch(term){
+
+		YTSearch({key:API_KEY, term: term}, (videos) => {
+		this.setState({videos:videos,selectedVideo:videos[0]});
+		});
+	}
+	render() {
+		//const videoSearch = _.debounce( term => {this.videoSearch(term) }, 300);
+	return (
+		<div>
+			<SearchBar onSearchTermChange={term => this.videoSearch(term)}/>
+			<VideoDetail video={this.state.selectedVideo} />
+			<VideoList
+				onVideoSelect={selectedVideo => this.setState({selectedVideo})} 
+				videos={this.state.videos}/>
+		</div>
+		);
+	}
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'));
+
